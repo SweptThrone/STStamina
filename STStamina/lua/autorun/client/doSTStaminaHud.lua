1c6ce94f561271e629af319a9d5e1f3fb046a636
SweptThrone <ejvogt5@gmail.com>
2018-07-25 17:21:15 -0500

    added lerp

--- STStamina/lua/autorun/client/doSTStaminaHud.lua
+++ STStamina/lua/autorun/client/doSTStaminaHud.lua
@@ -1,15 +1,17 @@
+local smoothStam = 100
+
 hook.Add( "HUDPaint", "DrawStamina", function()
 	
 	local ply = LocalPlayer()
 	
 	--color calculations based on SDM's HUD
-	local hp = ply:GetNWFloat( "CurrStamina" )
+	local stam = ply:GetNWFloat( "CurrStamina" )
 	local g, r = 0, 0
-	if hp >= 50 then
+	if stam >= 50 then
 		g = 255
-		r = 255 - (5.1 * (hp - 50))
-	elseif hp < 50 then
-		g = (5.1 * (hp))
+		r = 255 - (5.1 * (stam - 50))
+	elseif stam < 50 then
+		g = (5.1 * (stam))
 		r = 255
 	else
 		g = 255
@@ -18,6 +20,8 @@ hook.Add( "HUDPaint", "DrawStamina", function()
 	
 	--draw stamina bar and background of stamina bar
 	draw.RoundedBox( 4, 48, ScrH() - 162, 312, 40, Color(0,0,0) )
-	draw.RoundedBox( 4, 54, ScrH() - 156, hp * 3, 28, Color(r,g,0) )
+	--thanks CodeBlue!
+	smoothStam = Lerp( 15 * FrameTime(), smoothStam, stam )
+	draw.RoundedBox( 4, 54, ScrH() - 156, smoothStam * 3, 28, Color(r,g,0) )
 	
 end )
